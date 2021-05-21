168b508f9c46d065f16a770d338f6b544a0e8372
Bianca <84011677+TripleBdesign@users.noreply.github.com>
2021-05-21 16:11:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+console.log("Hello Winc Academy");
+let name = 'Bianca';
+console.log(name);
+
+//variabele met if
+
+
+
+
+//opdracht 7 nummers optellen
+let tellen1 = 4;
+let tellen2 = 4;
+console.log(tellen1 + tellen2);
+
+//opdracht 7 strings optellen
+// Dit kan niet want dan schrijft hij de 2 waardes achter elkaar, de uitkomst is dan 44.
+
+
+let tellen3 = 2;
+let tellen4 = 5;
+let tellen5 = 8;
+
+//opdracht 8 addition +
+console.log(tellen5 + tellen1);
+
+//opdracht 8 subtraction -
+console.log(tellen5 - tellen3);
+
+//opdracht 8 multiplication *
+console.log(tellen4 * tellen1);
+
+//opdracht 8 division /
+console.log(tellen5 / tellen3);
+
+//opdracht 8 modulus %
+console.log(tellen5 % 10);
+
+//opdracht 9 variabele met leeftijd in cijfers
+let leeftijd = 46;
+console.log(leeftijd);
+// de typeof bij deze opdracht geeft 'number'.
+
+// opdracht 10 leeftijd in tekst
+let leeftijd1 = "46";
+console.log(leeftijd1);
+// dit is de typeof string
+
+//opdracht 11 begrijp ik niet, zoals ik het nu begrijp  moet ike en kopie maken van mijn huidige script file en deze een andere naam geven.
+//de script file heette eerst index.js en nu script.js.
